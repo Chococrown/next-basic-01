8d5f866d742ce76180b7b8cc728bee23342f6648
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-03-22 22:26:52 +0700

    สร้าง Logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,19 @@
 import Link from "next/link"
-
+import Image from "next/image"
 
 export default function Navbar(){
     return(
         <>
             <nav>
+
                 <div>
-                    <h1>Prasert Shop</h1>
+                    <Image src="/logo.png" width={50} height={50} alt="Logo"/>
                 </div>
                 
                 <Link href="/">หน้าแรก</Link>
                 <Link href="/about">เกี่ยวกับเรา</Link>
                 <Link href="/products">สินค้าทั้งหมด</Link>
+                
             </nav>
         </>
     )
