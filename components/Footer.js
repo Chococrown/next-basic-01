e9a16edd71309f0dc9eeb8578d3991ec7a059a79
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-03-22 21:19:38 +0700

    สร้าง Components

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <Footer>
+            copyright 2025 | PrasertShop
+        </Footer>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return(
+        <>
+            <nav>
+                <div>
+                    <h1>PrasertShop</h1>
+                </div>
+                <ul>
+                    <li>หน้าแรก</li>
+                    <li>เกี่ยวกับเรา</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
