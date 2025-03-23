b7dee32d2f957d535799121d5d0c997f786e0e87
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-03-23 09:46:11 +0700

    about

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,16 @@
+import styles from "@/styles/about.module.css"
+import Image from "next/image";
+
 export default function About(){
     return(
         <>
-            <h1>เกี่ยวกับเรา</h1>
-        </>
+            <div className={styles.containner}>
+    
+            <h1 className={styles.title}>เกี่ยวกับเรา</h1>
+    
+            <Image src="/about.svg" width={400} height={400} alt="logo" />
+    
+            </div>
+      </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import styles from "@/styles/Home.module.css"
+import styles from "@/styles/home.module.css"
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,7 +9,11 @@ export default function Home() {
 
         <h1 className={styles.title}>หน้าแรกของเว็บไชต์</h1>
 
-        <Image src="/shopping.svg" width={300} height={300} />
+        <Image src="/shopping.svg" width={400} height={400} alt="logo" />
+
+        <p>ยินดีต้อนรับสู่ร้านค้า Prasert</p>
+
+        <Link href="/products" className={styles.btn}>ดูสินค้าทั้งหมด</Link>
 
       </div>
     </>
