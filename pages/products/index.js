99a0e03e543941195f0742c466d0eb14da3ca467
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-03-24 20:26:33 +0700

    ดึงรูปภาพมาใช้งาน

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,7 @@
-//https://dummyjson.com/products?limit=12
+import Image from 'next/image';
 
 export async function getStaticProps() {
-    const res = await fetch("https://dummyjson.com/products?limit=12")
+    const res = await fetch("https://dummyjson.com/products?limit=10")
     const data = await res.json()
     console.log(data)
 
@@ -14,6 +14,17 @@ export default function Index({products}){
     return(
         <>
             <h1>สินค้าทั้งหมด</h1>
+
+            <div>
+                {products.map(item=>(
+                    <div key={item.id}>
+                        <h3>{item.title}</h3>
+                        <Image src={item.thumbnail} width={300} height={300} alt={item.title}/>
+
+
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
